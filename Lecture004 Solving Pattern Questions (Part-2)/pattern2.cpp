377303e025acd478dadf836887a0393cfd14690f
Jagrat2004 <jagratagrawal67@gmail.com>
2026-06-28 18:21:33 +0530

    pattern 2

--- Lecture004 Solving Pattern Questions (Part-2)/pattern2.cpp
+++ Lecture004 Solving Pattern Questions (Part-2)/pattern2.cpp
@@ -1,23 +1,17 @@
 #include<iostream>
 using namespace std;
-
-int main() {
+int main(){
     int n;
     cin>>n;
-
     int i = 1;
-    int count = 1;
-    while(i<=n) {
+    while(i <= n){
+        int j =1;
+        while(j<= n){
+            cout << j;
+            j = j+1;
 
-        int j=1;
-        while(j<=n) {
-            cout<<count<<" ";
-            count = count  + 1;
-            j = j + 1;
         }
         cout<<endl;
-        i = i +1;
-
+        i=i+1;
     }
-    return 0;
 }
