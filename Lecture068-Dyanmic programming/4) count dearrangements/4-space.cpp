27e0a6e79f08ab55c9cbcff00a4a1afc9675bbeb
Jagrat2004 <jagratagrawal67@gmail.com>
2026-08-12 07:56:46 +0530

    DP-6

--- Lecture068-Dyanmic programming/4) count dearrangements/4-space.cpp
+++ Lecture068-Dyanmic programming/4) count dearrangements/4-space.cpp
@@ -4,19 +4,20 @@ using namespace std;
 const long long int MOD = 1e9 + 7;  
 
 long long int solve(int n) {  
-    vector<long long int>dp(n+1,0);
-    dp[1] = 0;
-    dp[2] = 1;
+    
+    long long int prev2= 0;
+    long long int prev1 = 1;
     for(int i = 3; i<=n;i++){
-        long long int first = dp[i-1]%MOD;
-        long long int second = dp[i-2]%MOD;
+        long long int first = prev1 % MOD;
+        long long int second = prev2 % MOD;
         long long int sum = (first+second)%MOD;
 
         long long int ans = ((i-1)*sum)%MOD;
-        dp[i] = ans;
+        prev2 = prev1;
+        prev1 = ans;
 
     }
-    return dp[n];
+    return prev1;
 }
 
 
