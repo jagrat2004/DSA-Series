ab955252610923b1dc5ea091b43feed4b5609e9a
Jagrat2004 <jagratagrawal67@gmail.com>
2026-08-16 08:43:54 +0530

    subsequences

--- Lecture037 Recursion Day7/subsequences.cpp
+++ Lecture037 Recursion Day7/subsequences.cpp
@@ -1,23 +1,48 @@
-void solve(vector<string>& ans, string str, string output, int i) {
-    //base case
-    if(i>=str.length()) {
-        if(output.length()>0)
-        ans.push_back(output);
-        return ;
-        }
-    
-    //exclude
-    solve(ans, str, output, i+1);
-    //include
-    output.push_back(str[i]);
-    solve(ans, str, output, i+1);
-}
+#include <bits/stdc++.h>
+using namespace std;
 
-vector<string> subsequences(string str){
-	
-	vector<string> ans;
-    string output = "";
-    solve(ans,str,output,0);
-    return ans;
-	
+class Solution {
+public:
+
+
+    void solve(vector<string>&ans, string str, string output, int i){
+
+        //base case
+        if(i>=str.length()){
+            if(output.length() > 0){
+                ans.push_back(output);
+            }
+            return;
+        }
+
+        // Exclude current character
+        solve(ans,str,output, i+1);
+
+        // Include current character
+        output.push_back(str[i]);
+        solve(ans,str,output, i+1);
+    }
+
+    vector<string> subsequences(string str){
+        vector<string>ans;
+        string output = "";
+
+        solve(ans,str,output,0);
+        return ans;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    string str;
+    cin >> str;
+
+    vector<string> ans = obj.subsequences(str);
+
+    for (string s : ans) {
+        cout << s << endl;
+    }
+
+    return 0;
 }
