a5a3dff6ddeb510f0f1a77b81f8186365689fb6f
Jagrat2004 <jagratagrawal67@gmail.com>
2026-08-21 09:31:46 +0530

    insertionsort

--- Lecture018 Insertion Sort/insertionSort.cpp
+++ Lecture018 Insertion Sort/insertionSort.cpp
@@ -1,23 +1,41 @@
+#include<iostream>
 #include<vector>
 using namespace std;
 
-void insertionSort(int n, vector<int> &arr){
-    
-    for(int i = 1; i<n; i++) {
+
+
+void insertSortAlgo(int n, vector<int>&arr){
+    for(int i = 1; i<n; i++){
         int temp = arr[i];
         int j = i-1;
-        for(; j>=0; j--) {
-            
-            if(arr[j] > temp) {
+        for(; j>=0; j--){
+            if(arr[j] > temp){
                 //shift
                 arr[j+1] = arr[j];
-            }
-            else { // ruk jao
+            }else{
                 break;
             }
-            
         }
-        //copy temp value
-        arr[j+1] = temp;  
-    } 
+        arr[j+1] = temp;
+    }
+}
+
+int main()
+{
+    vector<int> arr = {10, 1, 7, 4, 8, 2, 11, 6};
+
+    int n = arr.size();
+
+    insertSortAlgo(n, arr);
+
+    cout << "Sorted array: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
 }
