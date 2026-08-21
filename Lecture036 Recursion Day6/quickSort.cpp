b7998126ead16b755e673b7763e8cfa0917a4cd1
Jagrat2004 <jagratagrawal67@gmail.com>
2026-08-21 08:58:59 +0530

    Quick-sort

--- Lecture036 Recursion Day6/quickSort.cpp
+++ Lecture036 Recursion Day6/quickSort.cpp
@@ -2,60 +2,52 @@
 using namespace std;
 
 
-int partition( int arr[], int s, int e) {
 
+
+int partitions(int arr[], int s, int e){
     int pivot = arr[s];
 
     int cnt = 0;
-    for(int i = s+1; i<=e; i++) {
-        if(arr[i] <=pivot) {
+    for(int i= s+1; i<=e; i++){
+        if(arr[i]<=pivot){
             cnt++;
         }
     }
-
-    //place pivot at right position
-    int pivotIndex = s + cnt;
-    swap(arr[pivotIndex], arr[s]);
-
+    //place pivot at right position of the array
+    int pivoIndex = cnt + s;
+    swap(arr[pivoIndex], arr[s]);
+    
     //left and right wala part smbhal lete h 
     int i = s, j = e;
 
-    while(i < pivotIndex && j > pivotIndex) {
-
-        while(arr[i] <= pivot) 
-        {
+    while(i<pivoIndex && j<pivoIndex){
+        while(arr[i]<= pivot){
             i++;
         }
-
-        while(arr[j] > pivot) {
+        while(arr[j] > pivot){
             j--;
         }
-
-        if(i < pivotIndex && j > pivotIndex) {
+        if(i<pivoIndex && j>pivoIndex){
             swap(arr[i++], arr[j--]);
         }
-
     }
-
-    return pivotIndex;
-
+    return pivoIndex;
 }
 
-void quickSort(int arr[], int s, int e) {
 
+
+void quickSortAlgo(int arr[], int s, int e){
     //base case
-    if(s >= e) 
-        return ;
+    if(s>=e)
+     return;
 
-    //partitioon karenfe
-    int p = partition(arr, s, e);
+    int p = partitions(arr,s,e);
 
-    //left part sort karo
-    quickSort(arr, s, p-1);
+    //left part sort
+    quickSortAlgo(arr,s,p-1);
 
-    //right wala part sort karo
-    quickSort(arr, p+1, e);
-
+    //right part sort
+    quickSortAlgo(arr,p+1,e);
 }
 
 int main() {
@@ -63,7 +55,7 @@ int main() {
     int arr[10] = {2,4,1,6,9 ,9,9,9,9,9};
     int n = 10;
 
-    quickSort(arr, 0, n-1);
+    quickSortAlgo(arr, 0, n-1);
 
     for(int i=0; i<n; i++) 
     {
