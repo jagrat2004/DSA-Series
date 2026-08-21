cd472c523f1863e0c2a728fa2d1c1a515bea04ba
Jagrat2004 <jagratagrawal67@gmail.com>
2026-08-21 08:37:37 +0530

    Rec-D5

--- Lecture035 Recursion Day5/mergeSort.cpp
+++ Lecture035 Recursion Day5/mergeSort.cpp
@@ -1,8 +1,7 @@
 #include<iostream>
 using namespace std;
 
-void merge(int *arr, int s, int e) {
-
+void merge(int *arr, int s, int e){
     int mid = (s+e)/2;
 
     int len1 = mid - s + 1;
@@ -11,62 +10,55 @@ void merge(int *arr, int s, int e) {
     int *first = new int[len1];
     int *second = new int[len2];
 
-    //copy values
-    int mainArrayIndex = s;
-    for(int i=0; i<len1; i++) {
-        first[i] = arr[mainArrayIndex++];
+    //copy values in new arr
+    int minIndexArr = s;
+    for(int i = 0; i<len1; i++){
+       first[i] = arr[minIndexArr]++;
     }
 
-    mainArrayIndex = mid+1;
-    for(int i=0; i<len2; i++) {
-        second[i] = arr[mainArrayIndex++];
+    minIndexArr = mid+1;
+    for(int i = 0; i<len2; i++){
+        second[i] = arr[minIndexArr]++;
     }
 
-    //merge 2 sorted arrays     
+    //merge 2 sorted arrays
     int index1 = 0;
     int index2 = 0;
-    mainArrayIndex = s;
+    minIndexArr = s;
 
-    while(index1 < len1 && index2 < len2) {
-        if(first[index1] < second[index2]) {
-            arr[mainArrayIndex++] = first[index1++];
+    while(index1 < len1 && index2 < len2){
+        if(first[index1] < second[index2]){
+            arr[minIndexArr++] = first[index1++];
+        }else{
+            arr[minIndexArr++] = second[index2++];
+
         }
-        else{
-            arr[mainArrayIndex++] = second[index2++];
-        }
-    }   
-
-    while(index1 < len1) {
-        arr[mainArrayIndex++] = first[index1++];
     }
-
-    while(index2 < len2 ) {
-        arr[mainArrayIndex++] = second[index2++];
+    while(index1 < len1){
+        arr[minIndexArr++] = first[index1++];
     }
-
+    while(index2 < len2){
+        arr[minIndexArr++] = second[index2++];
+    }
     delete []first;
     delete []second;
-
 }
 
-void mergeSort(int *arr, int s, int e) {
-
+void mergeSortAlgo(int *arr, int s, int e){
     //base case
-    if(s >= e) {
+    if(s>=e){
         return;
     }
-    
     int mid = (s+e)/2;
 
-    //left part sort karna h 
-    mergeSort(arr, s, mid);
-    
-    //right part sort karna h 
-    mergeSort(arr, mid+1, e);
+    //left part sort
+    mergeSortAlgo(arr,s,mid);
+
+    //right part sort
+    mergeSortAlgo(arr,mid+1,e);
 
     //merge
-    merge(arr, s, e);
-
+    merge(arr,s,e);
 }
 
 int main() {
@@ -74,7 +66,7 @@ int main() {
     int arr[15] = {3,7,0,1,5,8,3,2,34,66,87,23,12,12,12};
     int n = 15;
 
-    mergeSort(arr, 0, n-1);
+    mergeSortAlgo(arr, 0, n-1);
 
     for(int i=0;i<n;i++){
         cout << arr[i] << " ";
--- Lecture037 Recursion Day7/subsets.cpp
+++ Lecture037 Recursion Day7/subsets.cpp
@@ -1,30 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 private:
-    void solve(vector<int> nums, vector<int> output, int index, vector<vector<int> >& ans) {
-        //base case
-        if(index >= nums.size()) {
+    void solve(vector<int> nums, vector<int> output, int index,
+               vector<vector<int>>& ans) {
+
+        // Base case
+        if (index >= nums.size()) {
             ans.push_back(output);
-            return ;
+            return;
         }
-        
-        //exclude
-        solve(nums, output, index+1, ans);
-        
-        //include
+
+        // Exclude current element
+        solve(nums, output, index + 1, ans);
+
+        // Include current element
         int element = nums[index];
         output.push_back(element);
-        solve(nums, output, index+1, ans);
-        
+
+        solve(nums, output, index + 1, ans);
     }
-    
+
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        
-        vector<vector<int> > ans;
+
+        vector<vector<int>> ans;
         vector<int> output;
         int index = 0;
+
         solve(nums, output, index, ans);
+
         return ans;
-        
     }
 };
+
+int main() {
+    Solution obj;
+
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    vector<vector<int>> ans = obj.subsets(nums);
+
+    // Print all subsets
+    for (const auto& subset : ans) {
+        cout << "[";
+        
+        for (int i = 0; i < subset.size(); i++) {
+            cout << subset[i];
+
+            if (i != subset.size() - 1)
+                cout << ",";
+        }
+
+        cout << "]" << endl;
+    }
+
+    return 0;
+}
