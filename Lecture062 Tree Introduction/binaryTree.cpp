39624b7893a41bd6ca9b42bcbff3182c6367740f
Jagrat2004 <jagratagrawal67@gmail.com>
2026-09-23 07:31:52 +0530

    tree

--- Lecture062 Tree Introduction/binaryTree.cpp
+++ Lecture062 Tree Introduction/binaryTree.cpp
@@ -1,17 +1,16 @@
 #include<iostream>
-#include<queue>
 using namespace std;
 class node{
-   public:
-   int data;
-   node* left;
-   node* right;
+    public:
+    int data;
+    node*left;
+    node*right;
 
-   node(int d){
-    this->data = d;
-    this->left = nullptr;
-    this->right = nullptr;
-   }
+    node(int d){
+        this->data = d;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
 };
 
 node* buildTree(node* root){
@@ -22,43 +21,15 @@ node* buildTree(node* root){
     if(data == -1){
         return nullptr;
     }
-    cout<<"enter the data to insert in the left"<<endl;
+    cout<<"enter data for inserting in left"<<endl;
     root->left = buildTree(root->left);
-    cout<<"enter the data to insert in the right"<<endl;
+    cout<<"enter data for inserting in right"<<endl;
     root->right = buildTree(root->right);
     return root;
+
 }
-
-void levelOrderTraversal(node* root){
-  queue<node*>q;
-  q.push(root);
-  q.push(nullptr);
-
-  while(!q.empty()){
-    node* temp = q.front();
-    q.pop();
-
-    if(temp == nullptr){
-        cout<<endl;
-        if(!q.empty()){
-            q.push(nullptr);
-        }
-    }
-    else{
-        cout<<temp->data<<endl;
-        if(temp->left){
-            q.push(temp->left);
-        }
-        if(temp->right){
-            q.push(temp->right);
-        }
-    }
-  }
-
-};
 int main(){
-   node* root = nullptr;
-   root = buildTree(root);
-   levelOrderTraversal(root);
-   return 0;
+    node * root = nullptr;
+    root = buildTree(root);
+    return 0;
 }
